348f2f101ff55c12ed35ec68ce394f0e24ba335c
Aaditya Ramrame <161333408+Aadityaramrame@users.noreply.github.com>
2025-04-27 17:08:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from summarizer.translation_module import TextTranslator
+from summarizer.translator_module import TextTranslator
 import os
 
 app = Flask(__name__)
